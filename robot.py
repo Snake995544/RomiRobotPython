eaf5290ad13dbc2e29c9eae49d69fc25ea56af85
JSohn-1 <proepicgamer1@gmail.com>
2024-07-30 12:24:16 -0700

    created command-based drive

--- robot.py
+++ robot.py
@@ -5,52 +5,95 @@
 # the WPILib BSD license file in the root directory of this project.
 #
 
+#
+# Example that shows how to connect to a ROMI from RobotPy
+#
+# Requirements
+# ------------
+#
+# You must have the robotpy-halsim-ws package installed. This is best done via:
+#
+#    # Windows
+#    py -3 -m pip install robotpy[commands2,sim]
+#
+#    # Linux/macOS
+#    pip3 install robotpy[commands2,sim]
+#
+# Run the program
+# ---------------
+#
+# To run the program you will need to explicitly use the ws-client option:
+#
+#    # Windows
+#    py -3 robotpy sim --ws-client
+#
+#    # Linux/macOS
+#    python robotpy sim --ws-client
+#
+# By default the WPILib simulation GUI will be displayed. To disable the display
+# you can add the --nogui option
+#
+
+import os
+import typing
+
 import wpilib
-import wpilib.drive
-import phoenix5
-import constants
-import math
+import commands2
 
-class MyRobot(wpilib.TimedRobot):
+from robotcontainer import RobotContainer
+
+# If your ROMI isn't at the default address, set that here
+os.environ["HALSIMWS_HOST"] = "10.0.0.2"
+os.environ["HALSIMWS_PORT"] = "3300"
+
+
+class MyRobot(commands2.TimedCommandRobot):
     """
-    This is a demo program showing the use of the DifferentialDrive class.
-    Runs the motors with arcade steering.
-    """  
+    Command v2 robots are encouraged to inherit from TimedCommandRobot, which
+    has an implementation of robotPeriodic which runs the scheduler for you
+    """
 
-    def robotInit(self):
-        """Robot initialization function"""
-        self.pitch_motor = phoenix5.WPI_TalonFX(constants.PITCH_MOTOR_ID)
-        self.POVUpCheck = False
+    autonomousCommand: typing.Optional[commands2.Command] = None
 
-        left_motor_1 = phoenix5.WPI_TalonSRX(constants.LEFT_MOTOR_1_ID)
-        left_motor_2 = phoenix5.WPI_TalonSRX(constants.LEFT_MOTOR_2_ID)
-        right_motor_1 = phoenix5.WPI_TalonSRX(constants.RIGHT_MOTOR_1_ID)
-        right_motor_2 = phoenix5.WPI_TalonSRX(constants.RIGHT_MOTOR_2_ID)
-        left_motor_1.follow(left_motor_2)
-        right_motor_1.follow(right_motor_2)
-        self.robot_drive = wpilib.drive.DifferentialDrive(left_motor_1, right_motor_1)
-        self.stick = wpilib.Joystick(constants.DRIVE_JOYSTICK_PORT)
+    def robotInit(self) -> None:
+        """
+        This function is run when the robot is first started up and should be used for any
+        initialization code.
+        """
 
-        # We need to invert one side of the drivetrain so that positive voltages
-        # result in both sides moving forward. Depending on how your robot's
-        # gearbox is constructed, you might have to invert the left side instead.
-        right_motor_1.setInverted(True)
+        # Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+        # autonomous chooser on the dashboard.
+        self.container = RobotContainer()
 
-    # def RobotPeriodic(self):
-    #     # Drive with arcade drive.
-    #     # That means that the Y axis drives forward
-    #     # and backward, and the X turns left and right.
-    #     self.robot_drive.arcadeDrive(self.stick.getY(), self.stick.getX())
-        # self.left_motor_1.set(self.stick.getY())
+    def disabledInit(self) -> None:
+        """This function is called once each time the robot enters Disabled mode."""
 
+    def disabledPeriodic(self) -> None:
+        """This function is called periodically when disabled"""
 
-    def teleopPeriodic(self):
-        # Drive with arcade drive.
-        # That means that the Y axis drives forward
-        # and backward, and the X turns left and right.
-        self.robot_drive.arcadeDrive(self.stick.getY(), self.stick.getZ())
-        if self.stick.getPOV(0) != -1 :
-            self.pitch_motor.set(math.cos(self.stick.getPOV(0) * math.pi / 180))
-        else :
-            self.pitch_motor.set(0)
-            
+    def autonomousInit(self) -> None:
+        """This autonomous runs the autonomous command selected by your RobotContainer class."""
+        self.autonomousCommand = self.container.getAutonomousCommand()
+
+        if self.autonomousCommand:
+            self.autonomousCommand.schedule()
+
+    def autonomousPeriodic(self) -> None:
+        """This function is called periodically during autonomous"""
+        commands2.CommandScheduler.getInstance().run()
+
+    def teleopInit(self) -> None:
+        # This makes sure that the autonomous stops running when
+        # teleop starts running. If you want the autonomous to
+        # continue until interrupted by another command, remove
+        # this line or comment it out.
+        if self.autonomousCommand:
+            self.autonomousCommand.cancel()
+
+    def teleopPeriodic(self) -> None:
+        """This function is called periodically during operator control"""
+        commands2.CommandScheduler.getInstance().run()
+
+    def testInit(self) -> None:
+        # Cancels all running commands at the start of test mode
+        commands2.CommandScheduler.getInstance().cancelAll()
