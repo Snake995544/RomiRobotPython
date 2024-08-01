48c996ea0111e5393f3bf105e8ab9584f0d6ee36
Snake995544 <jackson.lynch100@gmail.com>
2024-08-01 11:45:30 -0700

    fixed teleop (hopefully)

--- commands/arcadedrive.py
+++ commands/arcadedrive.py
@@ -3,11 +3,11 @@ import typing
 import commands2
 
 class ArcadeDrive(commands2.Command):
-    def __init__(self, drive:Drivetrain, forward: typing.Callable[[], float], rotation: typing.Callable[[], float]):
+    def __init__(self, drive:Drivetrain):
         super().__init__()
         self.drive = drive
-        self.forward = forward
-        self.rotation = rotation
+        self.forward = typing.Callable[[], float]
+        self.rotation = typing.Callable[[], float]
         self.addRequirements(self.drive)
     def execute(self) -> None:
         self.drive.drive(self.forward, self.rotation)
