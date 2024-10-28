9aa373d474b1f440ec845e5585e87936cec4cad6
Snake995544 <jackson.lynch100@gmail.com>
2024-10-28 16:46:57 -0700

    Create arcadedrive.py

---
+++ commands/arcadedrive.py
@@ -0,0 +1,14 @@
+# Arcade drive
+import wpilib
+import typing
+import commands2
+from subsystems.drivetrain import Drivetrain
+class ArcadeDrive(commands2.Command):
+    def __init__(self, drive: Drivetrain, forward: typing.Callable[[], float], rotation: typing.Callable[[], float]) -> None:
+        super().__init__()
+        self.drive = drive
+        self.forward = forward
+        self.rotation = rotation
+        self.addRequirements(self.drive)
+    def execute(self) -> None:
+        self.drive.arcade_drive(self.forward(), self.rotation())
